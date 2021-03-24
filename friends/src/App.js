ff893f2b06a9faac3bd4980e383d4f0ce9634e70
bukit3point0 <bukit3point0@gmail.com>
2021-03-24 00:59:23 -0400

    logout implemented

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 
 // Pages
+import Header from './components/Header'
 import Home from './components/Home'
 import Friends from './components/Friends'
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <Switch>
 
         <Route exact path='/'>
